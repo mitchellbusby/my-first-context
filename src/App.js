bd4e5d72684a325915efc1b859d43b03dbcb56f3
Mitchell Busby <mitchbusby@gmail.com>
2018-02-28 20:42:14 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import createReactContext from 'create-react-context';
+
+const MyContext = createReactContext();
+
+class ContextProvider extends Component {
+  state = { foo: 'bar '};
+  
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        actions: {
+          flick: () => this.setState({foo: 'zar'})
+        }
+      }}>
+        { this.props.children }
+      </MyContext.Provider>
+    );
+  }
+}
+
+const SecondComponent = () => {
+  return (
+    <MyContext.Consumer>
+    {
+      ({state, actions}) => (
+        <div>
+          <p>{ state.foo }</p>
+          <div>
+            <button onClick={actions.flick}>Change it!</button>
+          </div>
+        </div>
+      )
+    }
+    </MyContext.Consumer>
+  )
+}
+
+const FirstComponent = () => {
+  return (
+    <div>
+      <SecondComponent />
+    </div>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContextProvider>
+          <FirstComponent />
+        </ContextProvider>
+      </div>
+    );
+  }
+}
+
+export default App;
